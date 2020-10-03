441a8c3ff39422cd5cb152790e79e77a11a5e4dc
DrinoSan <s.becirbegovic@gmail.com>
2020-10-03 05:36:24 +0200

    First Day of DOYA

--- Assignment_Lastname_StudentID.py
+++ Assignment_Lastname_StudentID.py
@@ -8,14 +8,27 @@ from framework import BaseGame
 
 # Recommended Start: init function of Block Class
 class Block:
-    blocknames = ['clevelandZ', 'rhodeIslandZ', 'blueRicky', 'smashBoy', 'orangeRicky', 'teewee', 'hero']
+    blocknames = [
+        "clevelandZ",
+        "rhodeIslandZ",
+        "blueRicky",
+        "smashBoy",
+        "orangeRicky",
+        "teewee",
+        "hero",
+    ]
+
     def __init__(self, game, block_name):
-        self.name = None  # TODO set name / Can be 'hero', 'teewee', ...
-        self.rotation = 0  # TODO randomize rotation (e.g. 0, 1, 2, 3; Hint: different number of rotations per block)
+        self.name = random.choice(
+            self.blocknames
+        )  # TODO set name / Can be 'hero', 'teewee', ...
+        self.rotation = random.randint(
+            0, 3
+        )  # TODO randomize rotation (e.g. 0, 1, 2, 3; Hint: different number of rotations per block)
         self.set_shape(game.block_list[self.name][self.rotation])
         self.x = int(game.board_width / 2) - int(self.width / 2)
         self.y = 0
-        self.color = None  # TODO Set Color correctly / Can be 'red', 'green', ... (see self.blockColors)
+        self.color = "purple"  # TODO Set Color correctly / Can be 'red', 'green', ... (see self.blockColors)
 
     def set_shape(self, shape):
         self.shape = shape
@@ -31,7 +44,6 @@ class Block:
         pass
 
 
-
 class Game(BaseGame):
     def run_game(self):
         self.board = self.get_empty_board()
@@ -43,13 +55,7 @@ class Game(BaseGame):
         # TODO Fill in the score dictionary
         #  Maps "lines removed" to "raw points gained"
         #  0 lines: 0 points; 1 line: 40 points; 2 lines: 100 points; 3 lines: 300 points; 4 lines: 1200 points
-        self.score_dictionary = {
-            0: 0,
-            1: 40,
-            2: 100,
-            3: 300,
-            4: 1200
-        }
+        self.score_dictionary = {0: 0, 1: 40, 2: 100, 3: 300, 4: 1200}
 
         # GameLoop
         while True:
@@ -70,7 +76,10 @@ class Game(BaseGame):
     # Check if Coordinate given is on board (returns True/False)
     def is_coordinate_on_board(self, x, y):
         # TODO check if coordinate is on playingboard (in boundary of self.boardWidth and self.boardHeight)
-        return False
+        if x >= 0 and y >= 0 and x <= self.board_width and y <= self.board_height:
+            return True
+        else:
+            return False
 
     # Parameters block, x_change (any movement done in X direction), yChange (movement in Y direction)
     # Returns True if no part of the block is outside the Board or collides with another Block
@@ -82,19 +91,23 @@ class Game(BaseGame):
     # Returns True if the line is complete
     def check_line_complete(self, y_coord):
         # TODO check if line on yCoord is complete and can be removed
-        return False
+        return False if all(e != "." for e in self.board[y_coord]) else True
 
     # Go over all lines and remove those, which are complete
     # Returns Number of complete lines removed
     def remove_complete_line(self):
         # TODO go over all lines and check if one can be removed
-        return 0
+        for i in range(self.board_height, -1):
+            if all(e != "." for e in self.board[i]) == True:
+                for elem in self.board[i]:
+                    self.board[i][elem] = "."
 
     # Create a new random block
     # Returns the newly created Block Class
     def get_new_block(self):
         # TODO make block choice random! (Use random.choice out of the list of blocks) see blocknames array
-        blockname = None
+        blockname = random.choice(Block.blocknames)
+        print(blockname)
         block = Block(self, blockname)
         return block
 
@@ -104,20 +117,21 @@ class Game(BaseGame):
         pass
 
     # calculate new Score after a line has been removed
-    def calculate_new_score(self, lines_removed, level):
-        # TODO calculate new score
+    def calculate_new_score(self, lines_removed, level):  # TODO calculate new score
         # Points gained: Points per line removed at once times the level modifier!
         # Points per lines removed corresponds to the score_directory
         # The level modifier is 1 higher than the current level.
         pass
 
     # calculate new Level after the score has changed
-        # TODO calculate new level
+    # TODO calculate new level
     def calculate_new_level(self, score):
         # The level generally corresponds to the score divided by 300 points.
         # 300 -> level 1; 600 -> level 2; 900 -> level 3
         # TODO increase gamespeed by 1 on level up only
-        pass
+        if score % 300 == 0:
+            level = score / 300
+            return level
 
     # set the current game speed
     def set_game_speed(self, speed):
@@ -125,22 +139,23 @@ class Game(BaseGame):
         # It starts as defined in base.py and should increase by 1 after a level up.
         pass
 
-#-------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------
 # Do not modify the code below, your implementation should be done above
-#-------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 def main():
     pygame.init()
     game = Game()
 
     game.display = pygame.display.set_mode((game.window_width, game.window_height))
     game.clock = pygame.time.Clock()
-    pygame.display.set_caption('Tetris')
+    pygame.display.set_caption("Tetris")
 
-    game.show_text('Tetris')
+    game.show_text("Tetris")
 
     game.run_game()
-    game.show_text('Game Over')
+    game.show_text("Game Over")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
